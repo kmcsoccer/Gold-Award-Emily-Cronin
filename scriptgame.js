c7becd86d53588453050ed42c31eb31b2833d4bb
kmcsoccer <68297831+kmcsoccer@users.noreply.github.com>
2020-07-16 11:29:05 -0700

    Update scriptgame.js

--- scriptgame.js
+++ scriptgame.js
@@ -1,14 +1,20 @@
-/* global createCanvas, frameRate, background*/
+/* global createCanvas, frameRate, background, loadImage, image*/
 
-var img
+let img;
+function preload() {
+  img = loadImage('WormClipart.jpg');
+}
 
 function setup(){
   createCanvas(800, 500);
   frameRate(75);
   background(245, 230, 162);
-  img = loadimage("WormClipart.jpg");
+  background(50);
+  // Top-left corner of the img is at (10, 10)
+  // Width and height are 50 x 50
+  image(img, 10, 10, 50, 50);
 }
 
 function draw(){
-  image(img, 0, 0);
+
 }
